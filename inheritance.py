9fcc4ad3a8fe2713a950ff798b729f10c0fd9f21
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-14 12:34:10 +0530

    cpmpleted task

---
+++ inheritance.py
@@ -0,0 +1,82 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("Riddhi", "Disha")
+x.printname()
+
+# Create a Child Class
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Alpa", "Mukesh")
+x.printname()
+
+# Use the super() Function
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Heer", "Pruthvi")
+x.printname()
+
+# Add Properties
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = lname
+
+x = Student("Riddhi", "Gorasiya", 2025)
+print(x.graduationyear)
+
+# Add Methods
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Riddhi", "Gorasiya", 2025)
+x.welcome()
