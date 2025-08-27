052e2817ccb8d25fbce3062139dc31ac5e557c17
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-27 15:56:57 +0530

    completed inser

---
+++ week-3/day-15.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+# Connect to SQLite database
+conn = sqlite3.connect('sqlite.db')
+cursor = conn.cursor()
+# Create table
+cursor.execute("""CREATE TABLE STUDENT3(NAME VARCHAR(255), CLASS VARCHAR(255), SECTION VARCHAR(255))""")
+# Insert data into the table
+cursor.execute("INSERT INTO STUDENT VALUES ('riddhi', '7th', 'A')")
+cursor.execute("INSERT INTO STUDENT VALUES ('disha', '8th', 'B')")
+cursor.execute("INSERT INTO STUDENT VALUES ('heni', '9th', 'C')")
+print("Data Inserted in the table: ")
+cursor.execute("SELECT * FROM STUDENT")
+for row in cursor.fetchall():
+    print(row)
+conn.commit()
+conn.close()
