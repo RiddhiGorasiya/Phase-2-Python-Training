7d7593765eb548917d31157255f7956729db6daa
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-27 16:22:49 +0530

    completed insert

--- week-3/day-15.py
+++ week-3/day-15.py
@@ -4,7 +4,7 @@ import sqlite3
 conn = sqlite3.connect('sqlite.db')
 cursor = conn.cursor()
 # Create table
-cursor.execute("""CREATE TABLE STUDENT3(NAME VARCHAR(255), CLASS VARCHAR(255), SECTION VARCHAR(255))""")
+cursor.execute("""CREATE TABLE STUDENT(NAME VARCHAR(255), CLASS VARCHAR(255), SECTION VARCHAR(255))""")
 # Insert data into the table
 cursor.execute("INSERT INTO STUDENT VALUES ('riddhi', '7th', 'A')")
 cursor.execute("INSERT INTO STUDENT VALUES ('disha', '8th', 'B')")
@@ -15,6 +15,3 @@ for row in cursor.fetchall():
     print(row)
 conn.commit()
 conn.close()
-
-
-
