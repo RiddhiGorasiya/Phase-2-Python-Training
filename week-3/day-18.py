52c2e8c39b97321c0b085013b1102789d2127fa2
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-29 13:03:12 +0530

    start day-18

--- week-3/day-18.py
+++ week-3/day-18.py
@@ -22,3 +22,29 @@ import requests
 r = requests.put("https://httpbin.org/put", data = {'a': 1, 'b': 2})
 print("Stsatus code:", r.status_code)
 print("Response Body:", r.content.decode())
+
+# delete method
+import requests 
+r = requests.delete("https://httpbin.org//delete", data = {'key': 'value'})
+print(r) 
+print(r.json())
+
+# head method
+import requests
+r = requests.head("https://httpbin.org/get", data = {'key': 'value'})
+print(r)
+print(r.headers)
+print(r.content)
+
+# patch method
+import requests
+r = requests.patch("https://httpbin.org/patch", data = {'key': 'value'}) 
+print(r)
+print(r.content) 
+
+# response object
+import requests
+response = requests.get('https://api.github.com/')
+print(response.url)
+print(response.status_code)  
+
