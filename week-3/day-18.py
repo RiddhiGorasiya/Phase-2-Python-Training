aa730d2acde5ae3c3cd473bfd8eeedc3a40a66e4
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-29 17:59:43 +0530

    completed day-18

--- week-3/day-18.py
+++ week-3/day-18.py
@@ -2,7 +2,7 @@ import requests
 response = requests.get("https://www.geeksforgeeks.org/")
 print(response.status_code)
 
-# get method
+# # get method
 import requests
 response = requests.get("https://github.com/RiddhiGorasiya/Phase-2-Python-Training.git")
 print(response.status_code)
@@ -46,5 +46,42 @@ print(r.content)
 import requests
 response = requests.get('https://api.github.com/')
 print(response.url)
-print(response.status_code)  
+print(response.status_code)
 
+# authentication using python requests
+import requests
+from requests.auth import HTTPBasicAuth
+response = requests.get('https://api.github.com/user', auth=HTTPBasicAuth('username', 'password'))
+print(response.status_code) 
+
+# SSL Certificate Verification
+import requests
+response = requests.get('https://expired.badssl.com/', verify=False)
+print(response.status_code)
+
+# providing a custom certificate
+import requests
+response = requests.get('https://github.com/', verify='/path/to/certfile')
+print(response.status_code)
+
+
+# session objects
+import requests
+s = requests.Session()  
+session = s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
+r = s.get('https://httpbin.org/cookies')
+print(r.text)
+
+# error handling with requests
+import requests 
+try:
+    response = requests.get("https://www.example.com/", timeout=5)
+    response.raise_for_status()  
+except requests.exceptions.HTTPError as http_errh:
+    print("HTTP Error:", http_errh)
+except requests.exceptions.ConnectionError as conn_errc:
+    print("Error Connecting:", conn_errc)
+except requests.exceptions.Timeout as time_errt:
+    print("Timeout Error:", time_errt)
+except requests.exceptions.RequestException as req_err:
+    print("Something went wrong:", req_err)
